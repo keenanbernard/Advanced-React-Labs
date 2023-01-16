7d84dc86a29b83a3ea228417661f528dcfe9dce5
kbernard <k.bernard@belizetelemedia.net>
2023-01-15 20:45:11 -0600

    Week 2 - Completed Lab 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Goals from "./Components/Week 2/Lab 1 - useState Hook/GoalForm";
+import GiftCard from "./Components/Week 2/Lab 1 - useState Hook/GiftCard";
 
 
 function App() {
   return (
     <div className="App">
-      <Goals />
+      <GiftCard />
     </div>
   );
 }
